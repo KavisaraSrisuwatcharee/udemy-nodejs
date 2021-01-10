d51b5efbf9b6ed401aae22ebce00acff5ea16a47
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-01-11 00:01:50 +0700

    [ADD] web servers

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require("request");
 
 const forecast = (latitude, longtitude, callback) => {
-    const URL = 'http://api.weatherstack.com/current?access_key=568f8bd89e9a8ba98aeb4113a52492b5&query=' + latitude + ',' + longtitude +
+    const url = 'http://api.weatherstack.com/current?access_key=568f8bd89e9a8ba98aeb4113a52492b5&query=' + latitude + ',' + longtitude +
         '&units=f'
-    request({ url: URL, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to wether service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, "It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "% chance of rain");
+            callback(undefined, "It is currently " + body.current.temperature + " degrees out. There is a " + body.current.precip + "% chance of rain");
 
         }
     })
